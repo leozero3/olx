7e55f7659850468d0568b9988a6c3bae4e6b5fdc
leozero3 <realleo2009@hotmail.com>
2020-12-02 20:47:23 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/inc/real/olx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.inc.real.olx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:olx/home.dart';
+
+void main() async{
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+
+  runApp(MaterialApp(
+    title: 'OLX',
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
