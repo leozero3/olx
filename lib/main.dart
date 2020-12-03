4ddaeb7a9736f73c87bf6de1d5c5c68baee473bf
leozero3 <realleo2009@hotmail.com>
2020-12-02 21:46:43 -0300

    login UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:olx/home.dart';
 
+
+final ThemeData temaPadrao = ThemeData(
+  primaryColor: Color(0xff9c27b0),
+  accentColor: Color(0xff7b1fa2)
+);
+
+
 void main() async{
   // WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
@@ -9,6 +16,7 @@ void main() async{
   runApp(MaterialApp(
     title: 'OLX',
     home: Home(),
+    theme: temaPadrao,
     debugShowCheckedModeBanner: false,
   ));
 }
